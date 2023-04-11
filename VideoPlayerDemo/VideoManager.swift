232b326b0f1d2b332c054ef46eb525c13c855e04
Clarissa Calderon <clarissa.calderon9@gmail.com>
2023-04-11 12:25:07 -0700

    fix

--- VideoPlayerDemo/VideoManager.swift
+++ VideoPlayerDemo/VideoManager.swift
@@ -5,6 +5,7 @@
 //  Created by Clarissa Calderon on 4/10/23.
 //
 
+import Combine
 import Foundation
 
 enum Query: String, CaseIterable {
@@ -14,25 +15,24 @@ enum Query: String, CaseIterable {
 class VideoManager: ObservableObject {
     
     @Published private(set) var videos: [Video] = []
-    @Published var selectedQuery: Query = .nature {
-        didSet {
-            Task.init {
-                await findVideos(topic: selectedQuery)
-            }
-        }
-    }
+    @Published var selectedQuery: Query = .nature
     
     init() {
-        Task.init {
-            await findVideos(topic: selectedQuery)
-        }
+        $selectedQuery
+            .removeDuplicates()
+            .await { query in
+                await self.findVideos(topic: query)
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+            .assign(to: &$videos)
     }
     
-    func findVideos(topic: Query) async {
-        do {
-            guard let url = URL(string: "https://api.pexels.com/videos/search?query=\(topic)&per_page=10&orientation=portrait"
+    func findVideos(topic: Query) async -> [Video] {
+        guard let url = URL(string: "https://api.pexels.com/videos/search?query=\(topic)&per_page=10&orientation=portrait"
 ) else { fatalError("Missing URL") }
-            
+        
+        do {
             var urlRequest = URLRequest(url: url)
             urlRequest.setValue(Constants.pexelApiKey, forHTTPHeaderField: "Authorization")
             
@@ -44,12 +44,24 @@ class VideoManager: ObservableObject {
             decoder.keyDecodingStrategy = .convertFromSnakeCase
             let decodedData = try decoder.decode(ResponseBody.self, from: data)
             
-            DispatchQueue.main.async {
-                self.videos.removeAll()
-                self.videos = decodedData.videos
-            }
+            return decodedData.videos
         } catch {
             print("Error fetching data from Pexel: \(error)")
+            return []
         }
+    }
+}
+
+extension Publisher {
+    func `await`<T>(_ transform: @escaping (Output) async -> T) -> AnyPublisher<T, Failure> {
+        flatMap { value -> Future<T, Failure> in
+            Future { promise in
+                Task {
+                    let result = await transform(value)
+                    promise(.success(result))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
     }
 }
