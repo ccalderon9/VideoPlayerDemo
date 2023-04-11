79f786f2477015cfa03b8445089982f01f66a654
Clarissa Calderon <clarissa.calderon9@gmail.com>
2023-04-11 12:22:51 -0700

    asynchronous programming

---
+++ VideoPlayerDemo/DataModels.swift
@@ -0,0 +1,39 @@
+//
+//  DataModels.swift
+//  VideoPlayerDemo
+//
+//  Created by Clarissa Calderon on 4/11/23.
+//
+
+import Foundation
+
+struct ResponseBody: Decodable {
+    
+    var page: Int
+    var perPage: Int
+    var totalResults: Int
+    var url: String
+    var videos: [Video]
+}
+
+struct Video: Identifiable, Decodable {
+    var id: Int
+    var image: String
+    var duration: Int
+    var user: User
+    var videoFiles: [VideoFile]
+    
+    struct User: Identifiable, Decodable {
+        var id: Int
+        var name: String
+        var url: String
+    }
+    
+    struct VideoFile: Identifiable, Decodable {
+        var id: Int
+        var quality: String
+        var fileType: String
+        var link: String
+    }
+}
+
